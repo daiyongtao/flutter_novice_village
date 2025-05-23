444c95ec22c6f05ceb1b4553f6de587da6cdc065
daiyongtao <18811508093@163.com>
2025-05-23 19:23:18 +0800

    【Feature】新手村 - 猜数字，动画使用和状态周期

--- lib/guess/result_notice.dart
+++ lib/guess/result_notice.dart
@@ -2,54 +2,60 @@ import 'package:flutter/material.dart';
 
 // 可以通过自定义一个class，也可以通过函数直接返回一个Stack
 /*自定义view：背景颜色 + 中间大文字*/
-class ResultNotice extends StatelessWidget {
+class ResultNotice extends StatefulWidget {
   final String info;
   final Color color;
   const ResultNotice({super.key, required this.info, required this.color});
 
   @override
+  State<StatefulWidget> createState() => _ResultNoticeState();
+}
+
+class _ResultNoticeState extends State<ResultNotice> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 500)
+    );
+    animationController.forward(); // 启动动画器
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Expanded(child: Container(
         alignment: Alignment.center,
-        color: color,
-        child: Text(
-          info,
-          style: TextStyle(
-              fontSize: 50,
-              color: Colors.white
-          ),
-        )
-    ));
-
-    // 也上面效果一致，需要外部再包一层Expanded
-    return Stack(
-      children: [
-        Container(color: color),
-        Center(
-            child:  Text(
-              info,
+        color: widget.color,
+        child: AnimatedBuilder(
+            animation: animationController,
+            builder: (_, child) => Text(
+              widget.info,
               style: TextStyle(
-                  fontSize: 50,
-                  color: Colors.white
+                  fontSize: 54 * (animationController.value),
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
               ),
             )
         )
-      ],
-    );
+    ));
+  }
+
+  @override
+  void didUpdateWidget(covariant ResultNotice oldWidget) {
+    animationController.forward(from: 0);
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 }
 
-// 通过函数返回一个widget，和下面自定义ResultNotice效果一样
-Widget _buildResultNotice(Color color, String info) {
-  return Expanded(child: Container(
-      alignment: Alignment.center,
-      color: color,
-      child: Text(
-        info,
-        style: TextStyle(
-            fontSize: 50,
-            color: Colors.white
-        ),
-      )
-  ));
-}
+// StatefulWidget  和 StatelessWidget
+// 选取 StatefulWidget 的标准 -----：
+// 当前组件展示区域，在交互中需要改变内容；且外界无需在意内部状态数据
