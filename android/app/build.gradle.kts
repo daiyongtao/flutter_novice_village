4039cbdbd1862b251ba59948867518b128936c7e
daiyongtao <18811508093@163.com>
2025-05-26 10:55:15 +0800

    【Feature】新手村 - 修复 Android NDK version doesn't seem to be right for new projects 的警告问题

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.flutter_novice_village"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
